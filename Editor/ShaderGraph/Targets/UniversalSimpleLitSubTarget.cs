335ba6c245b8e1cd38da0698f0ec1edac65d463a
Mehmet Soyturk <mehmet.soyturk@qonic.com>
2025-07-26 23:25:03 +0300

    Removed reference to CoreKeywordDescriptors.ForwardPlus for Unity 6.1

--- Editor/ShaderGraph/Targets/UniversalSimpleLitSubTarget.cs
+++ Editor/ShaderGraph/Targets/UniversalSimpleLitSubTarget.cs
@@ -872,7 +872,8 @@ namespace UnityEditor.Rendering.Universal.ShaderGraph
                 { CoreKeywordDescriptors.LightLayers },
                 { CoreKeywordDescriptors.DebugDisplay },
                 { CoreKeywordDescriptors.LightCookies },
-#if UNITY_2022_2_OR_NEWER
+#if UNITY_6000_1_OR_NEWER
+#elif UNITY_2022_2_OR_NEWER
                 { CoreKeywordDescriptors.ForwardPlus },
 #else
                 { CoreKeywordDescriptors.ClusteredRendering },
